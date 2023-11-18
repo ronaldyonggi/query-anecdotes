21c84e5704ac3b4d2f363a5a2041284d44943380
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-18 15:28:33 +0800

    Initial app configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import './App.css'
+import Notification from './components/Notification'
+import AnecdoteForm from './components/AnecdoteForm'
 
 function App() {
 
+  const handleVote = anecdote => {
+    console.log('vote')
+  }
+
+  const anecdotes = [
+    {
+      "content": "If it hurts, do it more often",
+      "id": "47145",
+      "votes": 0
+    }
+  ]
+
   return (
+    <div>
+      <h3>Anecdote App</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote => 
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+        )}
+
+    </div>
   )
 }
 
