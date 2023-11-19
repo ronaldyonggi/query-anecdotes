3f841c8f32f3b918721682a8fde813b9384df67e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-19 15:50:39 +0800

    6.20 implement fetching anecdotes from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 
 function App() {
 
@@ -8,13 +10,31 @@ function App() {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    }
-  ]
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   }
+  // ]
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
 
   return (
     <div>
