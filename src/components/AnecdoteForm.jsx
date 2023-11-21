308b694db8cf3506717ff189eb863f458e8fbb5b
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-21 13:59:48 +0800

    6.23 add new anec triggers notification dispatch

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,7 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { createAnecdote } from "../requests"
+import { useNotificationDispatch } from "../NotificationContext"
 
 const AnecdoteForm = () => {
+    const notificationDispatch = useNotificationDispatch()
+
     const queryClient = useQueryClient()
     const newNoteMutation = useMutation({
         mutationFn: createAnecdote,
@@ -16,6 +19,10 @@ const AnecdoteForm = () => {
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
         newNoteMutation.mutate({content, votes: 0})
+        notificationDispatch({ type: true, payload: `anecdote '${content}' added` })
+        setTimeout(() => {
+            notificationDispatch({ type: false})
+        }, 5000);
     }
 
     return (
