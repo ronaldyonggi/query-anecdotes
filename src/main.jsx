10d2ddb43af05a448c445d39fb7558eceeaafc6e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-21 13:59:07 +0800

    6.23 wrap app with Notification context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from './App.jsx'
 import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NotificationContextProvider } from './NotificationContext.jsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <NotificationContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </NotificationContextProvider>
 )
